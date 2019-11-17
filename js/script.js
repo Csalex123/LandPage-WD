8f727f40005910c7885b7d7c2e11e2ef3c83afa2
Alex Ricardo <Alex.RiCardo1999@hotmail.com>
2019-11-17 13:46:17 -0300

    MENU AJUSTADO

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
-    $(".nav-toggle, nav-close").click(function(e){
-         e.preventDefautl();
+    $(".nav-toggle, .nav-close").click(function(e){
+         e.preventDefault();
          $('.nav').toggleClass('active');
     });
 });
